853069f28282108c15113c8561b97e9f2dc28173
matthewsd03 <matthewsd03@hotmail.com>
2021-10-25 20:59:47 +0100

    First Push

---
+++ Thief.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thief : Character
+{
+
+    Thief()
+    {
+        health -= 50;
+        mana -= 50;
+        stamina += 100;
+        characterName = "Pick";
+        bodyType = "ThiefBody";
+    }
+
+    void Start()
+    {
+        Attack _stab = gameObject.AddComponent<Stab>();
+        attacks.Add(_stab);
+        SetBody();
+    }
+}
+
